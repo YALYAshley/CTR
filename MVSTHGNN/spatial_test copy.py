38d27a246c85c89b75cfbe68d4f860b7434e9d07
YSLYAshley <zhixuanwuYSLYA@163.com>
2022-01-05 08:48:46 +0800

    train_default

--- MVSTHGNN/spatial_test copy.py
+++ MVSTHGNN/spatial_test copy.py
@@ -1,205 +1,259 @@
 from __future__ import absolute_import
 from __future__ import division
-import math
-from typing import Tuple
-# from mvpose.EasyMocap.easymocap.mytools.file_utils import select_nf
-from torch.nn.parameter import Parameter
 from HGNN import HGNN_conv
 from HGNN import construct_H_with_KNN, generate_G_from_H, hyperedge_concat
-from softmax import softmax
 from json2data import json2data
+from torchreid.metrics import metric
+from torchreid.utils.avgmeter import AverageMeter
 
-__all__ = ['MV_TSHL']
+# __all__ = ['MV_TSHL']
 
 import numpy as np
-import copy
 import torch
 from torch import nn
-from torch.nn import functional as F
-from sklearn.model_selection import train_test_split
-import torch.optim as optim
-from torchreid import models
-from torchreid import data_manager, metrics, lr_scheduler
 from torch.utils.data import DataLoader, Dataset
-import torch.utils.model_zoo as model_zoo
-from torch.autograd import Variable
-import os.path as osp
-import pandas as pd
-from pandas import Series,DataFrame
 
-from torchreid.utils.reidtools import calc_splits
-from torchreid.utils.torchtools import weights_init_kaiming, weights_init_classifier
+device = "cpu"
 
 class argparse():
     pass
 
+
 args = argparse()
-args.epochs, args.learning_rate, args.patience = [300, 0.001, 4]
-args.hidden_size, args.input_size= [40, 30]
-args.device, = [torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),]
+args.epochs, args.learning_rate, args.patience = [100, 0.001, 4]
+args.hidden_size, args.input_size = [40, 30]
+args.device, = [torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), ]
+train_root = '/home/mn/8T/code/new-hgnn/MVSTHGNN/data/train/json/'
+valid_root = '/home/mn/8T/code/new-hgnn/MVSTHGNN/data/valid/json/'
+multi_train_data, train_lbls = json2data(train_root)
+multi_valid_data, valid_lbls = json2data(valid_root)
 
-def gen_feature():
+def get_packed_data(multi_data_sum, multi_lbls, frame_st, frame_end):
 
-    '''
-    窗口宽度为2s
-    3个视角
-    每秒5帧
-    18个节点
-    超图网络输入顶点数为540
-    最终f_sum构成为：3（6s/2s），10帧（取2s，每秒5帧），8个动作，3个视角的同一个节点，每个节点包含（x,y,prob）
-    '''
-    f1 = []
-    f_sum = []
-    lbl_sum = []
-   
-    multi_data_sum=[]
-    windows = 2
-    frames = 5
+    packed_multi_data = []
+    packed_multi_lbl = []
+    interval_frame = 2
+    duration = (frame_end - frame_st) * 5
 
-    data1, len_pose_file, len_views, len_lbls = json2data()
+    for i in range(0, len(multi_data_sum), interval_frame):
+        data2 = multi_data_sum[i : i + duration]
+        lbl = multi_lbls[i : i + duration]
+        packed_multi_data.append(data2)
+        packed_multi_lbl.append(lbl)
 
-# The sliding window is 2s, data——>dict
-    for num in range(len_views):
-        multi_data = []
-        for i in range(0, len(data1), len_pose_file):
-            data2 = data1[i + 10 * num: i + 10 * num + (windows * frames)]
-            multi_data.append(data2)
-        multi_data_sum.append(multi_data)
-    # print(multi_data_sum[0])
-    # print(len(multi_data_sum[2]))
-    # print(len(multi_data_sum))
 
-    loop_num = int(len_pose_file/(windows * frames))
-    for n in range(loop_num):
-        f = []
-        lbl = []
-        for i in range(len_lbls):
-            for joint in range(0,18):
-                    for j in range(0,(windows * frames)):
-                        for view in range(0, len_views):
-                            f1 = multi_data_sum[n][view + 3*i][j]['data'][0,joint,:]
-                            lbl1 = multi_data_sum[n][view + 3*i][j]['action']
-                            # print(f1)
-                            f.append(f1) 
-                            lbl.append(lbl1)
+    return packed_multi_data, packed_multi_lbl
 
-        f_sum.append(f)
-        lbl_sum.append(lbl)
-    # print("f.size:",len(f_sum))
-    # print("lbl.size:",lbl_sum)
-    return f_sum, lbl_sum
 
-# def X_to_H(X):
-#     """
-#     calculate H from edge_list
-#     :param edge_dict: edge_list[i] = adjacent indices of index i
-#     :return: H, (n_nodes, n_nodes) numpy ndarray
-#     """
-#     n_edges = int(len(X)/3)
-#     n_nodes = int(len(X))
-#     H = np.zeros(shape=(n_nodes, n_edges))
-#     for i in range(0, n_nodes):
+def get_feature(data,label):
 
-#     print(H)
-#     return H
+    x = []
+    y = []
+    for i in range(len(data)):
+        for j in range(len(data[0])):  # 5
+            f1 = []
+            for joint in range(len(data[0][0][0])):  # 18 joints
+                f = []
+                for view in range(len(data[0][0])):  # 3 views
+                    X = data[i][j][view][joint]
+                    f.append(X)
+                f1.append(f)
+            x.append(f1)
+            lbl1 = label[i][j]
+            y.append(lbl1)
+    return x, y
 
-def get_train():
-    f_sum, lbl_sum = gen_feature()
-    f_sum = tuple(f_sum[0])
-    # lbl_sum = tuple(lbl_sum[0])
-    f_sum = torch.cat(f_sum, dim=0)
-    # print(f_sum[2])
-    # X = np.array(f_sum[0])
-    X = f_sum.reshape(-1,18,3,3)
-    # label = lbl_sum.reshape(-1,18,3,3)
-    # print("get_train_X:",X.shape)
-    # print("get_train_label:",label.shape)
+class MyDataSet(Dataset):
+    def __init__(self, flag='train'):
+        if flag == 'train':
+            frame_st = 0
+            frame_end = 2
+            duration = (frame_end - frame_st) * 5
+            self.data, self.label = get_packed_data(multi_train_data, train_lbls, frame_st, frame_end)
+            for i in range(duration):
+                self.data.pop()
+                self.label.pop()
+        else:
+            frame_st = 0
+            frame_end = 2
+            duration = (frame_end - frame_st) * 5
+            self.data, self.label = get_packed_data(multi_valid_data, valid_lbls, frame_st, frame_end)
+            for i in range(duration):
+                self.data.pop()
+                self.label.pop()
 
-    return X
+        self.length = len(self.data)
 
-def get_valid():
-    f_sum, _ = gen_feature()
-    f_sum = tuple(f_sum[1])
-    f_sum = torch.cat(f_sum, dim=0)
-    # print(f_sum[2])
-    # X = np.array(f_sum[0])
-    X = f_sum.reshape(-1,18,3,3)
-    # print("get_valid_X:",X.shape)
-    return X
+    def __getitem__(self, index):
+
+        data = self.data[index]
+        label = self.label[index]
+        return data, label
+
+    def __len__(self):
+        return self.length
 
 def gen_G(X):
+    G = []
 
-    G = [] 
-    for i in range(X.size(0)):
-        for j in range(0, len(X[0])): 
-            H = X[i][j] 
-            # print("H:",H)
+    for feature in X:  # iter over each batch
+        for ft in feature:
+            H = None
+            ft = tuple(ft)
+            ft = torch.cat(ft, dim=0)
+            ft = ft.reshape(-1, 3)
+            H_spatial = construct_H_with_KNN(ft, K_neigs=3,
+                                             is_probH=True,
+                                             m_prob=1, mode="global", total_node_num=len(feature), S=3)
+            H = hyperedge_concat(H, H_spatial)
             g = generate_G_from_H(H)
-            G.append(g)
-        G = torch.stack((G), dim=0)
-        # print("G.size:", G.size())
-        return G
+        G.append(g)
+    G = torch.stack((G), dim=0)
+    return G
 
 class FrameWiseHGNN(nn.Module):
-    def __init__(self, hids=[1, 36, 16], class_num= 8):
-        super(FrameWiseHGNN,self).__init__()
+    def __init__(self, hids=[3, 1], class_num=8):
+        super(FrameWiseHGNN, self).__init__()
+        self.activation = nn.Softmax(dim=1)
         # hgnn layers
-        self.layers = nn.ModuleList()
+        self.layers = nn.ModuleList()  # the list of hypergraph layers
         for j in range(1, len(hids)):
             self.layers.append(HGNN_conv(hids[j - 1], hids[j]))  # set hiddens
-
         self.cls_layer = nn.Linear(hids[-1], class_num)  # 设置全链接层，输入格式是1*8
-        print("cls_layer:",self.cls_layer)
-    
-    def forward(self, x):
-        layers = nn.ModuleList()
-        x = get_train()
-        G = gen_G(x)
-        f_in = torch.Tensor(x)
+        print("cls_layer:", self.cls_layer)
 
+    def forward(self, x):
+        layers = self.layers
+        f_in = gen_G(x)
         for L in layers:
-            f_in = L(f_in, G)  # 进入网络层
-        return f_in
+            f_in = L(f_in, f_in)  # 进入网络层
+        # n x c -> 1 x c
+        f_in = f_in.max(1)[0]
+        # 1 x c -> 1 x 8
+        out_feature = self.cls_layer(f_in)
+
+        # result = self.activation(out_feature)
+        result = out_feature
+        return result
+
 
 def train_it():
+    train_dataset = MyDataSet(flag='train')
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=10, shuffle=True)
+    valid_dataset = MyDataSet(flag='valid')
+    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=10, shuffle=True)
 
-    # images_train, lbls_train, images_valid, lbls_valid = gen_G(get_train()), gen_G(get_valid())
-    images_train, images_valid = gen_G(get_train()), gen_G(get_valid())
+    train_loss = []
+    valid_loss = []
+    train_epochs_loss = []
+    valid_epochs_loss = []
 
     net = FrameWiseHGNN()
- 
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(net.parameters(),lr=args.learning_rate)
-    print_freq = 100
-    num_epochs= 25
-
-    # optim criterion
+    # criterion = nn.NLLLoss()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.learning_rate)
     for epoch in range(args.epochs):
-        if epoch % print_freq == 0:
-            print('-' * 10)
-            print(f'Epoch {epoch}/{num_epochs - 1}')
         net.train()
-        train_out = net(images_train)
-        optimizer.zero_grad()
-        # loss = criterion(lbls_train,train_out)
-        # loss.backward()
-        optimizer.step()
-        train_pred = torch.argmax(train_out)
+        train_epoch_loss = []
 
-    # valid
-    net.eval()
-    valid_out = net(images_valid)
-    # loss = criterion(valid_out,lbls_valid)
-    valid_pred = torch.argmax(valid_out)
+        for idx, (data_x, data_y) in enumerate(train_dataloader, 0):
+            data = data_x.to(torch.float32).to(device)
+            lbl = data_y.to(torch.float32).to(device)
+            optimizer.zero_grad()
 
-    print("train_pred:",train_pred)
-    print("valid_pred:",valid_pred)
+            data_x, data_y = get_feature(data, lbl)
+            data_y = torch.Tensor(data_y).long()
+            data_y_acc = np.array(data_y)
 
-    acc = metrics.accuracy(train_pred,valid_pred)
-    print("accuracy:",acc)
+            # compute output
+            outputs = net(data_x)
+            loss = criterion(outputs, data_y)
+
+            #measure accuracy and record loss
+            train_pred = outputs.argmax(dim=1)
+            train_pred = np.array(train_pred)
+            confMatrix,label2idx = metric.buildConfusionMatrix(train_pred,data_y_acc)
+
+            # all_prediction = metric.calculate_all_prediction(confMatrix)
+            label_prediction = []
+            label_recall = []
+
+            for i in label2idx:
+                label_prediction.append(metric.calculate_label_prediction(confMatrix,label2idx[i]))
+                label_recall.append(metric.calculate_label_recall(confMatrix,label2idx[i]))
+                # for j in label2idx:
+                #     labelidx_i = label2idx[i]
+                #     label2idx_j = label2idx[j]
+                #     print('  ',confMatrix[labelidx_i][label2idx_j],end=' ')
+                # print('\n')
+        
+            # print('prediction(accuracy)=',all_prediction,'%')
+            # for ei,i in enumerate(label2idx):
+            #     print(ei,'\t',i,'\t','prediction=',label_prediction[ei],'%,\t recall=',label_recall[ei],'%,\tf1=',
+            #           metric.calculate_f1(label_prediction[ei],label_recall[ei]))
+            p = round(np.array(label_prediction).sum()/len(label_prediction),2)
+            r = round(np.array(label_recall).sum()/len(label_prediction),2)
+
+            loss.backward()
+            optimizer.step()
+            train_epoch_loss.append(loss.item())
+            train_loss.append(loss.item())
+            if idx % (len(train_dataloader) // 2) == 0:
+                print("train: epoch={}/{},{}/{}of train, loss={}".format(
+                    epoch, args.epochs, idx, len(train_dataloader), loss.item()))
+                print('prediction=', p, '%,recall=', r, '%,f1=', metric.calculate_f1(p, r))
+        train_epochs_loss.append(np.average(train_epoch_loss))
+
+
+        # =====================valid============================
+        net.eval()
+        valid_epoch_loss = []
+        for idx, (data_x, data_y) in enumerate(valid_dataloader, 0):
+            data = data_x.to(torch.float32).to(device)
+            lbl = data_y.to(torch.int32).to(device)
+
+            data_x, data_y = get_feature(data, lbl)
+            data_y = torch.Tensor(data_y).long()
+            data_y_acc = np.array(data_y)
+
+            outputs = net(data_x)
+            loss = criterion(outputs, data_y)
+
+            # measure accuracy and record loss
+            valid_pred = outputs.argmax(dim=1)
+            valid_pred = np.array(valid_pred)
+            confMatrix, label2idx = metric.buildConfusionMatrix(valid_pred, data_y_acc)
+
+            label_prediction = []
+            label_recall = []
+
+            for i in label2idx:
+                label_prediction.append(metric.calculate_label_prediction(confMatrix, label2idx[i]))
+                label_recall.append(metric.calculate_label_recall(confMatrix, label2idx[i]))
+
+            p = round(np.array(label_prediction).sum() / len(label_prediction), 2)
+            r = round(np.array(label_recall).sum() / len(label_prediction), 2)
+
+
+            valid_epoch_loss.append(loss.item())
+            valid_loss.append(loss.item())
+            if idx % (len(valid_dataloader) // 2) == 0:
+                print("valid: epoch={}/{},{}/{}of valid, loss={}".format(
+                    epoch, args.epochs, idx, len(valid_dataloader), loss.item()))
+                print('prediction=', p, '%,recall=', r, '%,f1=', metric.calculate_f1(p, r))
+        valid_epochs_loss.append(np.average(valid_epoch_loss))
+
+        # ====================adjust lr========================
+        lr_adjust = {
+            2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
+            10: 5e-7, 15: 1e-7, 20: 5e-8
+        }
+        if epoch in lr_adjust.keys():
+            lr = lr_adjust[epoch]
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+            print('Updating learning rate to {}'.format(lr))
+
 
 if __name__ == '__main__':
     train_it()
-    # gen_G(get_train())
-    # get_train()
-    # FrameWiseHGNN(hids=[1, 36, 16], class_num= 8)
